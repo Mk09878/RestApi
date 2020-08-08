11edfe0d9d48e1cb1ad8ccdd61291db8e737a6de
Mihir Kelkar <mihirk107@gmail.com>
2020-08-07 19:59:10 -0500

    Added two functionalities including updating and deleting a topic.

--- src/main/java/com/springboot/restapi/TopicController.java
+++ src/main/java/com/springboot/restapi/TopicController.java
@@ -1,9 +1,7 @@
 package com.springboot.restapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,17 +19,37 @@ public class TopicController {
      * Creates a list of Topic objects. Spring Boot automatically converts the return type into JSON objects.
      * The generated JSON has key names corresponding to instance variables of the Topic class
      * and the JSON values are the values of those variables.
-     * @return list of Topic objects
+     * @return List of Topic objects
      */
-    @RequestMapping("/topics")
+    @RequestMapping("/topics") // No need to specify GET request since, the default is GET
     public List<Topic> getAllTopics(){
         return topicService.getTopics();
     }
 
-    @RequestMapping("/topics/{foo}")
-    public Topic getTopic(@PathVariable("foo") String id){ // If name of variable in RequestMapping and PathVariable is different
+    //@RequestMapping("/topics/{foo}")
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){ // If name of variable in RequestMapping and PathVariable is different then public Topic getTopic(@PathVariable("foo") String id)
         return topicService.getTopic(id);
     }
 
+    /**
+     * Tells spring that the request payload will contain a JSON representation of the Topic instance.
+     * So, take the RequestBody and convert it to a Topic instance and pass it to addTopic when URL is mapped.
+     * @param topic Instance of Topic class
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/topics") // To specify POST request
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}") // To specify POST request
+    public void updateTopic(@PathVariable String id, @RequestBody Topic topic){
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){ // If name of variable in RequestMapping and PathVariable is different then public Topic getTopic(@PathVariable("foo") String id)
+        topicService.deleteTopic(id);
+    }
 
 }
--- src/main/java/com/springboot/restapi/TopicService.java
+++ src/main/java/com/springboot/restapi/TopicService.java
@@ -2,18 +2,19 @@ package com.springboot.restapi;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private final List<Topic> topics;
+    private List<Topic> topics;
     public TopicService() {
-        this.topics = Arrays.asList(
+        this.topics = new ArrayList<>(Arrays.asList(
                 new Topic("spring", "Spring Framework", "Spring Framework Description"),
                 new Topic("java", "Core Java", "Core Java Description"),
                 new Topic("javascript", "JavaScript", "JavaScript Description")
-        );
+        ));
     }
 
     public List<Topic> getTopics(){
@@ -26,5 +27,27 @@ public class TopicService {
                 return topic;
         }
         return new Topic("Null","Null","Topic not found");
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic updated_topic){
+        for(int i = 0; i < topics.size(); i++){
+            if(topics.get(i).getId().equals(id)) {
+                topics.set(i, updated_topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        for(int i = 0; i < topics.size(); i++){
+            if(topics.get(i).getId().equals(id)){
+                topics.remove(i);
+                break;
+            }
+        }
     }
 }
