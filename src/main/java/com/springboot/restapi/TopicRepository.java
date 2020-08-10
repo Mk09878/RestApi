bf8a33e2849c3c263d215860087efc19aafdb1c1
Mihir Kelkar <mihirk107@gmail.com>
2020-08-09 22:47:07 -0500

    Implemented CRUD operations on the mySQL database

--- src/main/java/com/springboot/restapi/TopicRepository.java
+++ src/main/java/com/springboot/restapi/TopicRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 // Spring has a CrudRepository which consists of predefined methods to perform CRUD operations. So we can define an interface and make it extend the CrudRepository interface.
 // The CrudRepository requires a generic type argument. So, the first argument specifies the class whose objects will be operated upon and the second argument specifies the datatype of the primary key
 public interface TopicRepository extends CrudRepository<Topic, String> {
-
-
-
 }
